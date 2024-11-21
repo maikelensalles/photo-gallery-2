b29ae9abe53d4b72731ec5877c8180a466354a45
Maikelen Salles <maiki09salles@gmail.com>
2024-11-21 20:37:26 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'photo gallery 2',
+  webDir: 'www'
+};
+
+export default config;
